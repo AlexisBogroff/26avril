4e5130a51e2216f42da75445858470f7d6790a3c
Alexis Bogroff <31806900+AlexisBogroff@users.noreply.github.com>
2025-04-26 10:22:19 +0200

    Add file main salutations

---
+++ main.py
@@ -0,0 +1,18 @@
+def salutations():
+    return "coucou"
+
+def salutations_personalise(prenom):
+    return f"Coucou {prenom}"
+
+def salutations_personalise_complete(prenom, nom):
+    return f"Coucou {prenom} {nom}"
+
+def salutations_personalise_default(prenom, nom='piu'):
+    return f"Coucou {prenom} {nom}"
+
+
+message = salutations()
+print(message)
+
+message = salutations_personalise("alexis")
+print(message)
